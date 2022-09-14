948ee84fac3696750c6df8dc1738d15d742d2910
Yao-Jiayi <108216173+Yao-Jiayi@users.noreply.github.com>
2022-09-14 22:31:08 +0800

    Update zipDecompression.py

--- zipDecompression.py
+++ zipDecompression.py
@@ -1,13 +1,33 @@
 import zipfile
+import sys
 
-path = input('Enter Path:')
-savePath = input('Enter Decompression Path:')
+def zipDecompression():
+    if len(sys.argv) <= 1:
+        path = input('Enter Path:')
+        savePath = input('Enter Decompression Path:')
 
-try:
-    z = zipfile.ZipFile(path, 'r')
-    z.extractall(path=savePath)
-    z.close()
-except:
-    print("Decompression Error")
-else:
-    print("Decompression Complete")
+        try:
+            z = zipfile.ZipFile(path, 'r')
+            z.extractall(path=savePath)
+            z.close()
+        except:
+            print("Decompression Error")
+        else:
+            print("Decompression Complete")
+        finally:
+            input()
+    else:
+        path = sys.argv[1]
+        savePath = sys.argv[2]
+
+        try:
+            z = zipfile.ZipFile(path, 'r')
+            z.extractall(path=savePath)
+            z.close()
+        except:
+            print("Decompression Error")
+        else:
+            print("Decompression Complete")
+
+if __name__ == "__main__":
+    zipDecompression()
